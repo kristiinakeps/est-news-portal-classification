a354b4570d0703f5b7b6322ca589d9e40633a65a
LAPTOP-9UTB6FGK\annal <anna.laanevali@stacc.ee>
2021-05-24 19:05:28 +0300

    Data scraping scripts for err and ohtuleht

--- scrapers/err.py
+++ scrapers/err.py
@@ -14,60 +14,72 @@ article_tags = []
 article_headers = []
 article_dates = []
 article_body = []
-article_link = []
+article_links = []
 
+links = []
 
-for i in range(7):
+for i in range(365):
     content = driver.page_source
     soup = BeautifulSoup(content)
     for a in soup.find('div', attrs={'class':'left-block'}).findAll('a', href=True):
-        article_link.append(a.get('href'))
+        links.append(a.get('href'))
 
     nupud = driver.find_elements_by_css_selector(".history-btn")
     actions = ActionChains(driver)
     actions.move_to_element(nupud[2])
     actions.click(nupud[2])
     actions.perform()
-    time.sleep(1)
+    time.sleep(0.5)
 
+count = len(links)
+current = 0
+for link in links:
+    current += 1
+    try:
+        driver.get(link)
+        article_soup = BeautifulSoup(driver.page_source)
+        article = None
+        time = None
 
-for i in article_link:
-    driver.get(i)
-    article_soup = BeautifulSoup(driver.page_source)
-    article = None
-    time = None
-    
+        if (article_soup.find('article', attrs={'class':"prime hasPhoto no-border"}) is not None): 
+            article = article_soup.find('article', attrs={'class':"prime hasPhoto no-border"})
+        elif (article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto no-border"}) is not None):
+            article = article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto no-border"})
+        elif (article_soup.find('article', attrs={'class':"prime hasPhoto hasGallery no-border"}) is not None):
+            article = article_soup.find('article', attrs={'class':"prime hasPhoto hasGallery no-border"})
+        elif (article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto hasGallery no-border"}) is not None):
+            article = article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto hasGallery no-border"})
+        else:
+            print("There is a problem with this article:", link)
+            continue
 
-    if (article_soup.find('article', attrs={'class':"prime hasPhoto no-border"}) is not None): 
-        article = article_soup.find('article', attrs={'class':"prime hasPhoto no-border"})
-    elif (article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto no-border"}) is not None):
-        article = article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto no-border"})
-    elif (article_soup.find('article', attrs={'class':"prime hasPhoto hasGallery no-border"}) is not None):
-        article = article_soup.find('article', attrs={'class':"prime hasPhoto hasGallery no-border"})
-    elif (article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto hasGallery no-border"}) is not None):
-        article = article_soup.find('article', attrs={'class':"prime hasMedia hasPhoto hasGallery no-border"})
-    else:
-        print("There is a problem with this article:", i)
-        continue
+        if(article.find('time', attrs={'class':"pubdate ng-binding"}) is not None) :
+            time = article.find('time', attrs={'class':"pubdate ng-binding"})['title']
+        elif(article.find('span', attrs={'class':"pubdate ng-binding"}) is not None):
+            time = article.find('span', attrs={'class':"pubdate ng-binding"})['title']
+        else:
+            print("There is a problem with time", link)
+            continue
+        header = article.find('h1').get_text()
+        tag =  article.find('div', attrs={'class':"category"}).get_text()
+        body = article.find('div', attrs={'class':"text flex-row"}).get_text()
 
-    if(article_soup.find('time', attrs={'class':"pubdate ng-binding"}) is not None):
-        time = article.find('time', attrs={'class':"pubdate ng-binding"})['title']
-    elif(article_soup.find('span', attrs={'class':"pubdate ng-binding"}) is not None):
-        time = article.find('span', attrs={'class':"pubdate ng-binding"})['title']
-    else:
-        print("There is a problem with time", i) 
-        continue
-    header = article.find('h1').get_text()
-    tag =  article.find('div', attrs={'class':"category"}).get_text()
-    body = article.find('div', attrs={'class':"text flex-row"}).get_text()
-    
-    portal.append('err')
-    article_tags.append(tag)
-    article_headers.append(header)
-    article_dates.append(time)
-    article_body.append(body)
+        portal.append('err')
+        article_dates.append(time)
+        article_tags.append(tag)
+        article_headers.append(header)
+        article_body.append(body)
+        article_links.append(link)
 
-    print(time)
+        print(time)
+        if current % 10 == 0:
+            print(f'{current} of {count} done')
+    except:
+        print(f'Error with page {link}, skipping')
+        df = pd.DataFrame({'portal_name':portal,'date':article_dates, 'tag':article_tags, 'link':article_links, 'header':article_headers, 'article':article_body}) 
+        df.to_csv("C:\\Users\\annal\\Documents\\NLP\\andmekraapimine\\bak_articles.csv", index=False, encoding='utf-8')
+        df = pd.DataFrame({'links':links})
+        df.to_csv("C:\\Users\\annal\\Documents\\NLP\\andmekraapimine\\links.csv", index=False, encoding='utf-8')
 
-df = pd.DataFrame({'portal_name':portal,'date':article_dates, 'tag':article_tags, 'link':article_link, 'header':article_headers, 'article':article_body}) 
+df = pd.DataFrame({'portal_name':portal,'date':article_dates, 'tag':article_tags, 'link':article_links, 'header':article_headers, 'article':article_body}) 
 df.to_csv("C:\\Users\\annal\\Documents\\NLP\\andmekraapimine\\articles.csv", index=False, encoding='utf-8')
--- scrapers/ohtuleht.py
+++ scrapers/ohtuleht.py
@@ -14,63 +14,111 @@ article_headers = []
 article_dates = []
 article_body = []
 article_link = []
+links = []
 tags = []
 
+def allow_cookies():
+    try:
+        time.sleep(5)
+        notifications = driver.find_element_by_id('onesignal-slidedown-cancel-button')
+        actions = ActionChains(driver)
+        actions.click(notifications)
+        actions.perform()
+    except:
+        print('cookie feil')
+
+def accept_conditions():
+    try:
+        time.sleep(5)
+        conditions = driver.find_element_by_css_selector("button.fc-button-consent")
+        actions = ActionChains(driver)
+        actions.click(conditions)
+        actions.perform()
+    except:
+        print('conditions feil')
+
+def log_in():
+    time.sleep(3)
+    element = driver.find_element_by_css_selector("a.button-digi.button-digi--login.no-mobile-show")
+    actions = ActionChains(driver)
+    actions.click(element)
+    actions.perform()
+    email_parool = driver.find_element_by_css_selector("form.page-modal--popup-form.modal-hint--form").find_elements_by_tag_name("input")
+    email_parool[0].send_keys('email@gmail.com')
+    email_parool[1].send_keys('password')
+    element = driver.find_element_by_css_selector("div.page-modal--login-submit")
+    actions = ActionChains(driver)
+    actions.click(element)
+    actions.perform()
+
 def scroll_webpage():
-    print('starting')
+    print('starting scrolling')
+    last_article_date = ''
     while True:
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-        print('sleeping')
-        print('slept')
+        time.sleep(3)
         article_dates = BeautifulSoup(driver.page_source).findAll(
             'div', attrs={'class': "article-unit--date"})
-        print('test')
-        for date in article_dates:
-            print(date)
-            if "19. aprill 2021" in date.get_text():
-                return
+        if "31. oktoober 2021" in last_article_date or last_article_date == article_dates[-1].getText():
+            return
+        last_article_date = article_dates[-1].getText()
+        print(last_article_date)
 
+
+accept_conditions()
+allow_cookies()
+log_in()
 scroll_webpage()
 
 content = driver.page_source
 soup = BeautifulSoup(content, features="html.parser")
 for a in soup.findAll('h6'):
-    article_link.append(a.findAll('a', href=True)[0].get('href'))
+    links.append(a.findAll('a', href=True)[0].get('href'))
 
-for i in article_link:
-    if 'https' in i:
-        url = i
-    else:
-        url = 'https://www.ohtuleht.ee' + i
-    print(url)
-    driver.get(url)
-    article = BeautifulSoup(driver.page_source, features="html.parser")
-    article_header = None
-    time = None
-    
+count = len(links)
+current = 0
+for i in links:
+    print('count on', count)
+    current += 1
+    try:
+        if 'https' in i:
+            url = i
+        else:
+            url = 'https://www.ohtuleht.ee' + i
+        driver.get(url)
+        article = BeautifulSoup(driver.page_source, features="html.parser")
+        article_header = None
+        time = None
 
-    if (article.find('h1') is not None): 
-        article_header = article.find('h1')
-    else:
-        print("There is a problem with this article header:", i)
 
-    if(article.find('div', attrs={'class': "details--inner"}) is not None):
-        time = article.find('div', attrs={'class': "details--inner"}).get_text().split(',')[1]
-    else:
-        print("There is a problem with time", i)
+        if (article.find('h1') is not None): 
+            article_header = article.find('h1')
+        else:
+            print("There is a problem with this article header:", i)
 
-    header = article.find('h1').get_text()
-    tag =  article.find('div', attrs={'class':"article-main--sections"}).get_text()
-    body = ''
-    for x in article.find('div', attrs={'class':"page-layout--inner"}).findAll('p'):
-        body += x.get_text()
-    
-    portal.append('ohtuleht')
-    print(time)
-    article_headers.append(header)
-    article_dates.append(time)
-    article_body.append(body)
-    tags.append(tag)
+        if(article.find('div', attrs={'class': "details--inner"}) is not None):
+            time = article.find('div', attrs={'class': "details--inner"}).get_text().split(',')[1]
+        else:
+            print("There is a problem with time", i)
+
+        header = article.find('h1').get_text()
+        tag =  article.find('div', attrs={'class':"article-main--sections"}).get_text()
+        body = ''
+        for x in article.find('article', attrs={'class':"page-layout--container"}).findAll('p'):
+            body += x.get_text()
+        
+
+        portal.append('ohtuleht')
+        article_headers.append(header)
+        article_dates.append(time)
+        article_body.append(body)
+        article_link.append(url)
+        tags.append(tag)
+        if current % 10 == 0:
+            print(f'{current} of {count} done')
+    except:
+        print(f'failed with article {i}')
+
 
 
 df = pd.DataFrame({
@@ -81,4 +129,6 @@ df = pd.DataFrame({
     'header': article_headers,
     'article': article_body,
 })
-df.to_csv("articles.csv", index=False, encoding='utf-8')
+
+df.to_csv("C:\\Users\\annal\\Documents\\NLP\\andmekraapimine\\articles_ohtuleht_uus.csv", index=False, encoding='utf-8')
+
